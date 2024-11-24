9e3d979730ad283ebeede169f1399821d1b175f0
Andrew Charneski <acharneski@gmail.com>
2024-11-24 10:48:44 -0500

    fx

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "aicoder-demotest"
+
+includeBuild("../jo-penai/")
+includeBuild("../SkyeNet/")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        kotlin("jvm") version "2.0.20" apply false
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.4.0")
+}
